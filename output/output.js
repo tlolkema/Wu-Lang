4ce64f2e79569c4d5f2699aa2a6e36568ae45344
Jeffrey <jeffrey@jeffreyzutt.nl>
2024-11-19 16:54:16 +0100

    Ooh baby I like it raw

--- output/output.js
+++ output/output.js
@@ -4,6 +4,8 @@ const methodman = () => {
     throw new Error("Wu-Lang Forever");
   } catch (error) {
     console.log(error);
+  } finally {
+    console.log("Wu-Lang is for the children!");
   }
 };
 methodman();
